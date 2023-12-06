dfb43eb5848901b7132f99fb897b4f430c03c852
GEISABEL <110138843+GEISABEL@users.noreply.github.com>
2023-12-05 22:08:07 -0300

    Adicionar arquivos de projeto.

---
+++ BibliotecaWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BibliotecaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace BibliotecaWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+        public DbSet<Livro> Livros { get; set; }
+    
+
+        }
+    }
---
+++ BibliotecaWeb/Models/Livro.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaWeb.Models
+{
+    public class Livro
+    {
+        [Key]
+        [DisplayName("Código")]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Título")]
+        [StringLength(60)]
+       public string Titulo { get; set; }
+
+        [DisplayName("Autor(es)")]
+        public string? Autor { get; set; }
+
+      
+    }
+}
